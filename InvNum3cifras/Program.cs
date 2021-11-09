1293fbb16d8fe25b2c46fd5ce6733f3077cd150e
EmmilioV <emmiliov.p@gmail.com>
2021-11-09 14:38:58 -0500

    first commit

---
+++ InvNum3cifras/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InvNum3cifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            do
+            {
+
+                Console.WriteLine("DIgite el número de tres cifras que desea invertir");
+                numero = int.Parse(Console.ReadLine());
+
+            } while (numero < 99 || numero > 999);
+
+            int numeroInv = invertirNumero(numero);
+            Console.WriteLine($"el numero invertido es: {numeroInv}");
+
+        }
+
+        public static int invertirNumero(int numero)
+        {
+            int residuo1 = numero % 10;
+            int resultado1 = numero / 10;
+            int residuo2 = resultado1 % 10;
+            int resultado2 = resultado1 / 10;
+
+            int numeroinvertido = residuo1*100 + residuo2*10 + resultado2;
+
+            return numeroinvertido;
+        }
+    }
+}
